25a97e9ab22cf889e10cf4b95d49c8ed8abb1a6c
Nathan <me@nathanherald.com>
2018-03-25 15:21:24 +0200

    Support `brew services`
    Includes a `plist` template. Can be used like:
    
    ```
    $ brew services start faktory
    $ brew services stop faktory
    $ brew services list
    ```

--- faktory.rb
+++ faktory.rb
@@ -57,6 +57,32 @@ class Faktory < Formula
     end
   end
 
+
+  plist_options :manual => "faktory"
+
+  def plist; <<~EOT
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{bin}/faktory</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>#{HOMEBREW_PREFIX}/sbin:/usr/sbin:/usr/bin:/bin:#{HOMEBREW_PREFIX}/bin</string>
+        </dict>
+      </dict>
+    </plist>
+    EOT
+  end
+
+
   test do
     shell_output("#{bin}/faktory -v", 0)
     shell_output("#{bin}/faktory-cli -v", 0)
